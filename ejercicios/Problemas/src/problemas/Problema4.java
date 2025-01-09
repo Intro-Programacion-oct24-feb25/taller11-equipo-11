55b78812a69ca931c0166db61164c5ab8eb4ef29
villa <bryan.m.villa.c@gmail.com>
2025-01-09 12:13:24 -0500

    solucion

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,6 +18,57 @@ public class Problema4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        String nombre;
+        String cedula;
+        System.out.printf("Menu;\n1.Valor luz\n2. Valor predio\n");
+        opcion =entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Ingrese el nombre del cliente");
+        nombre= entrada.nextLine();
+        System.out.println("Ingrese la cedula del cliente");
+        cedula=entrada.nextLine();
+        
+        switch (opcion) {
+            case 1:
+                calcularValorLuz(nombre,cedula);
+                break;
+            case 2:
+                calcularPredio(nombre,cedula);
+                break;
+            
+
+        }
+    }
+    public static void calcularValorLuz(String nombre, String cedula){
+        Scanner entrada = new Scanner(System.in);
+        double valorKilowatio;
+        int numeroKilowatios;
+        double totalPagar;
+        String cadena ="";
+        System.out.println("Ingrese el valor de Kilowatios");
+        valorKilowatio = entrada.nextDouble();
+        System.out.println("Ingrese el numero de kilowatios consumidos");
+        numeroKilowatios = entrada.nextInt();
+        totalPagar = valorKilowatio*numeroKilowatios;
+        cadena = String.format("Cliente %s con cedula %s debe cancelar el valor"
+                + "de $ %.2f", nombre, cedula,totalPagar);
+        System.out.printf("%s\n",cadena);
     }
     
+    public static void calcularPredio(String nombre, String cedula){
+        Scanner entrada = new Scanner(System.in);
+        double valorMueble;
+        double totalPredio;
+        String cadena; 
+        System.out.println("Ingrese el valor del bien inmueble");
+        valorMueble = entrada.nextDouble();
+        totalPredio = valorMueble * 0.02;
+        cadena = String.format("Cliente %s con cedula %s tiene un bien inmueble"
+                + "valorado en $ %.2f y tiene que pagar de predio $ %.2f"
+                , nombre, cedula,valorMueble,totalPredio);
+        System.out.printf("%s\n",cadena);
+        
+    }
 }
