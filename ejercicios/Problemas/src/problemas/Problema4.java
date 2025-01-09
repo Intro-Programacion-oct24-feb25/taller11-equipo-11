2325333b09634a6a3acf66fac29a05745c461f4d
villa <bryan.m.villa.c@gmail.com>
2025-01-09 12:29:52 -0500

    solucion

--- ejercicios/Problemas/src/problemas/Problema4.java
+++ ejercicios/Problemas/src/problemas/Problema4.java
@@ -22,53 +22,53 @@ public class Problema4 {
         int opcion;
         String nombre;
         String cedula;
-        System.out.printf("Menu;\n1.Valor luz\n2. Valor predio\n");
-        opcion =entrada.nextInt();
+        System.out.printf("Menu;\n1.Valor luz\n2.Valor predio\n");
+        opcion = entrada.nextInt();
         entrada.nextLine();
         System.out.println("Ingrese el nombre del cliente");
-        nombre= entrada.nextLine();
+        nombre = entrada.nextLine();
         System.out.println("Ingrese la cedula del cliente");
-        cedula=entrada.nextLine();
-        
+        cedula = entrada.nextLine();
+
         switch (opcion) {
             case 1:
-                calcularValorLuz(nombre,cedula);
+                calcularValorLuz(nombre, cedula);
                 break;
             case 2:
-                calcularPredio(nombre,cedula);
+                calcularPredio(nombre, cedula);
                 break;
-            
 
         }
     }
-    public static void calcularValorLuz(String nombre, String cedula){
+
+    public static void calcularValorLuz(String nombre, String cedula) {
         Scanner entrada = new Scanner(System.in);
         double valorKilowatio;
         int numeroKilowatios;
         double totalPagar;
-        String cadena ="";
+        String cadena = "";
         System.out.println("Ingrese el valor de Kilowatios");
         valorKilowatio = entrada.nextDouble();
         System.out.println("Ingrese el numero de kilowatios consumidos");
         numeroKilowatios = entrada.nextInt();
-        totalPagar = valorKilowatio*numeroKilowatios;
+        totalPagar = valorKilowatio * numeroKilowatios;
         cadena = String.format("Cliente %s con cedula %s debe cancelar el valor"
-                + "de $ %.2f", nombre, cedula,totalPagar);
-        System.out.printf("%s\n",cadena);
+                + "de $ %.2f", nombre, cedula, totalPagar);
+        System.out.printf("%s\n", cadena);
     }
-    
-    public static void calcularPredio(String nombre, String cedula){
+
+    public static void calcularPredio(String nombre, String cedula) {
         Scanner entrada = new Scanner(System.in);
         double valorMueble;
         double totalPredio;
-        String cadena; 
+        String cadena;
         System.out.println("Ingrese el valor del bien inmueble");
         valorMueble = entrada.nextDouble();
         totalPredio = valorMueble * 0.02;
         cadena = String.format("Cliente %s con cedula %s tiene un bien inmueble"
-                + "valorado en $ %.2f y tiene que pagar de predio $ %.2f"
-                , nombre, cedula,valorMueble,totalPredio);
-        System.out.printf("%s\n",cadena);
-        
+                + "valorado en $ %.2f y tiene que pagar de predio $ %.2f",
+                nombre, cedula, valorMueble, totalPredio);
+        System.out.printf("%s\n", cadena);
+
     }
 }
--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,32 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
-    
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] suma = new int[3][3];
+        for (int i = 0; i < informacion2.length; i++) {
+            for (int j = 0; j < informacion[i].length; j++) {
+                suma[i][j] = obtenerSuma(informacion[i][j],
+                        informacion2[i][j]);
+            }
+        }
+
+        String cadena = "";
+        for (int i = 0; i < suma.length; i++) {
+            for (int j = 0; j < suma[i].length; j++) {
+                cadena = String.format("%s%d\t", cadena, suma[i][j]);
+            }
+            cadena = String.format("%s\n", cadena);
+        }
+        System.out.println(cadena);
+    }
+
+    public static int obtenerSuma(int valor1, int valor2) {
+        int operacion;
+        operacion = valor1 + valor2;
+        return operacion;
+    }
+
 }
